25ea7699c7710f2240afa43dcc1d95545d1585f1
David Wallace <davidwallacem@hotmail.com>
2020-10-10 21:39:53 -0300

    Add project files.

---
+++ EFCore.WebApi/Models/Batalha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore.WebApi.Models
+{
+    public class Batalha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime DtFim { get; set; }
+        public ICollection<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
---
+++ EFCore.WebApi/Models/Heroi.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EFCore.WebApi.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public IdentidadeSecreta IdentidadeSecreta { get; set; }
+        public ICollection<Arma> Armas { get; set; }
+        public ICollection<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
